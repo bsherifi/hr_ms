867f8297845e1ee1f1c24212e2557a6a7f0e325f
GentiM <44003210+GentritM@users.noreply.github.com>
2019-06-25 11:32:27 +0200

    comment

--- emp_ms.sql
+++ emp_ms.sql
@@ -18,7 +18,7 @@
 --
 -- Table structure for table `applicant`
 --
-
+/*comment*/
 DROP TABLE IF EXISTS `applicant`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
